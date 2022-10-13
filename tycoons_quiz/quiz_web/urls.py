a3a8d3b1e4d39cb2f815891259ded8306de7a2c2
Devni-Charithma <devnicharithma99@gmail.com>
2022-10-13 22:44:01 +0530

    create quiz configuration
    base do not support
    
    Co-Authored-By: Yadawa Weerakoon <110886425+yadawa99@users.noreply.github.com>

--- tycoons_quiz/quiz_web/urls.py
+++ tycoons_quiz/quiz_web/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("addq/", views.add_quizes, name="addq"),
 ]
--- tycoons_quiz/quiz_web/views.py
+++ tycoons_quiz/quiz_web/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from . models import *
+from .forms import QuizForm, QuestionForm
 # Create your views here.
 
 def home(response):
     return render(response, 'quiz_web/home.html')
+
+def add_quizes(response):
+    if response.method == "POST":
+        form = QuizForm(data=response.POST)
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.save()
+            obj = form.instance
+            return render(response, "add_quizes.html", {'obj': obj})
+    else:
+        form = QuizForm()
+    return render(response, 'quiz_web/add_quizes.html')
