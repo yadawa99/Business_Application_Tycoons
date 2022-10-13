2b8116645580aa95d21eb602b1383f080153c574
vidura-nilupul <99791107+vidura-nilupul@users.noreply.github.com>
2022-10-13 20:03:12 +0530

    Update models.py
    defining quiz model

--- tycoons_quiz/quiz_web/models.py
+++ tycoons_quiz/quiz_web/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+import random
 # Create your models here.
+
+
+class Quiz(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.CharField(max_length=500)    
+    number_of_questions = models.IntegerField(default=1)
+    time = models.IntegerField(help_text="Duration of the quiz in seconds", default="1")
+    
+    def __str__(self):
+        return self.name
+
+    def get_questions(self):
+        return self.question_set.all()
