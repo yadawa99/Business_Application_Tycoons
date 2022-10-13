9afc6dc9d57be3cc3567f41b5897afe042b3fa48
Yadawa Weerakoon <saumyanikethani@gmail.com>
2022-10-13 11:47:11 +0530

    change some files

--- tycoons_quiz/tycoons_quiz/urls.py
+++ tycoons_quiz/tycoons_quiz/urls.py
@@ -1,3 +1,6 @@
+from django.urls import path
+from .views import *
+
 """tycoons_quiz URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +16,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from . import  views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
