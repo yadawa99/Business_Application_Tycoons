7fb6a6510dd7940e7f4bf94fb8dcb5b3137e3a14
Devni-Charithma <devnicharithma99@gmail.com>
2022-10-14 09:21:14 +0530

    add quiz page

--- tycoons_quiz/quiz_web/views.py
+++ tycoons_quiz/quiz_web/views.py
@@ -12,5 +12,14 @@ def home(response):
     return render(response, 'quiz_web/home.html')
 
 def add_quizes(response):
-    form = QuizForm()
+    if response.method=="POST":
+        form = QuizForm(data=response.POST)
+        
+        if form.is_valid():
+            quiz = form.save(commit=False)
+            quiz.save()
+            obj = form.instance
+            return render(response, "add_quizes.html", {'obj':obj})
+    else:
+        form=QuizForm()
     return render(response, 'quiz_web/add_quizes.html',{"form":form})
