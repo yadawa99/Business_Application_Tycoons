9bb807a5bc9b2c75d18f14d67c826341d048ecb7
Yadawa Weerakoon <110886425+yadawa99@users.noreply.github.com>
2022-10-14 16:17:37 +0530

    Update views.py

--- tycoons_quiz/register/views.py
+++ tycoons_quiz/register/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .forms import SignUpForm, LoginForm
+#from .forms import SignUpForm, LoginForm
 from django.contrib.auth import authenticate, login
 # Create your views here.
 
