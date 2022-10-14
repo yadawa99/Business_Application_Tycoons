d87685088272bc63a0bf668f44e0b4a79bdf202b
Vidura-nilupul <99791107+vidura-nilupul@users.noreply.github.com>
2022-10-14 20:24:43 +0530

    Update urls.py

--- tycoons_quiz/quiz_web/urls.py
+++ tycoons_quiz/quiz_web/urls.py
@@ -7,5 +7,19 @@ urlpatterns = [
     path("main/",views.main, name="main"),
     path("about_us/", views.about_us, name="about_us")
 
+    path("<int:myid>/", views.quiz, name="quiz"), 
+    path('<int:myid>/data/', views.quiz_data_view, name='quiz-data'),
+    path('<int:myid>/save/', views.save_quiz_view, name='quiz-save'),
+    
+    path("signup/", views.Signup, name="signup"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
+    
+  
+    path('add_question/', views.add_question, name='add_question'),  
+    path('add_options/<int:myid>/', views.add_options, name='add_options'), 
+    path('results/', views.results, name='results'),    
+    path('delete_question/<int:myid>/', views.delete_question, name='delete_question'),  
+    path('delete_result/<int:myid>/', views.delete_result, name='delete_result'),    
 
 ]
