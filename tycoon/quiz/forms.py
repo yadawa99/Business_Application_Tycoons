074832bc753d528c203446280f6c8d7fb308494f
Yadawa Weerakoon <110886425+yadawa99@users.noreply.github.com>
2022-10-15 14:56:08 +0530

    new files added

---
+++ tycoon/quiz/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Quiz, Question, Answer
+from django.contrib import admin
+
+
+class QuizForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        fields = ('name', 'desc', 'number_of_questions', 'time')
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question
+        fields = ('content', 'quiz')
+
